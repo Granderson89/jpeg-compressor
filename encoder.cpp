568a4c928ef8391d15df6c843d66d6191e56eaec
DESKTOP-H24B8E7\grant <40218994@live.napier.ac.uk>
2018-12-14 15:42:54 +0000

    Res 6400 image included

--- encoder.cpp
+++ encoder.cpp
@@ -280,7 +280,7 @@ int main(int arg_c, char *ppArgs[])
 	ofstream data("png_to_jpeg_oclbetter.csv", ofstream::out);
 	// Set up constant arguments
 	char *output = "output.jpg";
-	int resolutions[7] = {256, 512, 768, 1024, 1280, 2560, 5120};
+	int resolutions[8] = {256, 512, 768, 1024, 1280, 2560, 5120, 6400};
 	char *qualities[10] = {"10", "20", "30", "40", "50", "60", "70", "80", "90", "100"};
 	// Loop over qualities
 	for (char *quality : qualities)
